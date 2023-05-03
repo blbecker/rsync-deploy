ab9282488b02cbdb9c3dced74026c30765546d00
Benjamin Becker <benjamin.becker@teamnorthwoods.com>
2023-05-03 13:00:14 -0400

    Add deletion to the rsync step. Setup github actions to build and push to dockerhub.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,4 @@ echo "${INPUT_DEPLOYMENT_KEY}" | base64 -d >> "/root/.ssh/id_rsa" && \
     chmod 0700 "/root/.ssh" && \
     chmod 0600 "/root/.ssh/id_rsa"
 
-rsync -e 'ssh -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no' -avzhP "${INPUT_SOURCE_DIR}" "${INPUT_DEPLOYMENT_USER}"@"${INPUT_DESTINATION_HOST}":"${INPUT_DESTINATION_PATH}"
+rsync -e 'ssh -i /root/.ssh/id_rsa -o StrictHostKeyChecking=no' -avzhP --delete "${INPUT_SOURCE_DIR}" "${INPUT_DEPLOYMENT_USER}"@"${INPUT_DESTINATION_HOST}":"${INPUT_DESTINATION_PATH}"
